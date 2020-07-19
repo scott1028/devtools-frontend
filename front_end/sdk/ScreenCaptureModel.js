27619b5a28710d4702aa23562e897e3eab16537c
scott <mic1028002@gmail.com>
2020-07-19 10:25:25 +0800

    wip;

--- front_end/sdk/ScreenCaptureModel.js
+++ front_end/sdk/ScreenCaptureModel.js
@@ -90,12 +90,14 @@ export class ScreenCaptureModel extends SDKModel {
     console.log('Sync screen size', data, metadata, sessionId, this._agent); // NOTE: screen resize
     const clientSessionID = location.search.match(/^.*\/(.*)$/)[1];
     if (clientSessionID) {
+      // There are some extension installed in your chrome which will affetc the height value.
       const menuBarHeight = window.outerHeight - window.innerHeight;
       const borderWidth = window.outerWidth - window.innerWidth;
+      // const borderWidth = 200;
       this._agent._target._agents.Browser.getWindowForTarget(clientSessionID).then(windowId => {
         this._agent._target._agents.Browser.setWindowBounds(
           windowId,
-          { left: 0, top: 0, width: window.innerWidth + borderWidth, height: window.innerHeight + menuBarHeight, windowState: 'normal'},
+          { left: -borderWidth, top: -menuBarHeight, width: window.innerWidth + borderWidth, height: window.innerHeight + menuBarHeight, windowState: 'normal'},
           // { left: 0, top: 0, width: window.innerWidth, height: 600, windowState: 'normal'},
           // { left: 0, top: 0, width: window.innerWidth, height: window.outerHeight, windowState: 'normal'},
         );
